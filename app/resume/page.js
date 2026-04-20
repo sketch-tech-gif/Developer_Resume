445eba370987f16fb0779dfef79cbf3309ad2e6e
sketch-tech-gif <kangogo6074@gmail.com>
2026-04-20 12:13:38 +0300

    initial portfolio with resume

---
+++ app/resume/page.js
@@ -0,0 +1,13 @@
+export default function Resume() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>My Resume</h1>
+
+      <p>Click below to view my CV</p>
+
+      <a href="/resume.pdf" target="_blank">
+        View Resume (PDF)
+      </a>
+    </div>
+  );
+}
