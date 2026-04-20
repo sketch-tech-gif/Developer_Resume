14f2bba1ba81f1ad21ccea04a22e9b9277f4b107
sketch-tech-gif <kangogo6074@gmail.com>
2026-04-20 12:55:43 +0300

    redirect homepage to CV

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div style={{ height: "100vh", margin: 0 }}>
-      <iframe
-        src="/resume.pdf"
-        style={{
-          width: "100%",
-          height: "100vh",
-          border: "none"
-        }}
-      />
-    </div>
-  );
+  redirect("/resume.pdf");
 }
